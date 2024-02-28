39690c79aa9d104d3de097eb53590cf64a5382dc
alinaeb <54652337+alinaeb1@users.noreply.github.com>
2024-02-28 11:56:14 -0500

    TrapArm

--- src/main/java/frc/robot/subsystems/TrapArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/TrapArmSubsystem.java
@@ -1,17 +1,50 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TrapArmSubsystem extends SubsystemBase {
-  /** Creates a new TrapArmSubsystem. */
-  public TrapArmSubsystem() {}
+  private final DoubleSolenoid pneumaticCylinder;
+  private final CANSparkMax flipMotor;
+  private final CANSparkMax rotateMotor;
+  private final SparkPIDController flipMotorPIDController = flipMotor.getPIDController();
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  
+  public TrapArmSubsystem() {
+    pneumaticCylinder = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    flipMotor = new CANSparkMax(FLIP_MOTOR_ID, MotorType.kBrushless);
+    rotateMotor = new CANSparkMax(ROTATE_MOTOR_ID, MotorType.kBrushless);
+    flipMotorPIDController = flipMotor.getPIDController();
+    
+    flipMotorPIDController.setP(PID_P);
+    flipMotorPIDController.setI(PID_I);
+    flipMotorPIDController.setD(PID_D);
   }
+
+  public void grab() {
+    pneumaticCylinder.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void release() {
+    pneumaticCylinder.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void flipToPosition(double position) {
+    flipMotorPIDController.setReference(position, ControlType.kPosition);
+  }
+
+  public void rotate(double speed) {
+    rotateMotor.set(speed);
+  }
+  
 }
