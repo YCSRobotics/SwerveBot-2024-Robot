5d4e529ad7601ef7f5c8f4245bf05b8d575fdc12
alinaeb <54652337+alinaeb1@users.noreply.github.com>
2024-02-28 10:45:22 -0500

    Added the TrapArm Code

--- src/main/java/frc/robot/subsystems/HangerSubsystem.java
+++ src/main/java/frc/robot/subsystems/HangerSubsystem.java
@@ -4,7 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkBase.ControlType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
