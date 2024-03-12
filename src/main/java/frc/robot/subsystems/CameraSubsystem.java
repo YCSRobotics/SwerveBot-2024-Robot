1b76322917fca80033c842e8e226c1d81da848b7
DaveDWG <dg103462@gmail.com>
2024-03-12 11:56:45 -0700

    removed 1 camera

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -6,16 +6,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CameraSubsystem extends SubsystemBase {
   private UsbCamera camera1;
-  private UsbCamera camera2;
+  //private UsbCamera camera2;
 
   /** Creates a new ProximitySensorSubsystem. */
   public CameraSubsystem() {
     camera1 = CameraServer.startAutomaticCapture(0);
     camera1.setResolution(160, 120);
     camera1.setFPS(15);
-    camera2 = CameraServer.startAutomaticCapture(1);
-    camera2.setResolution(160, 120);
-    camera2.setFPS(15);
+    //camera2 = CameraServer.startAutomaticCapture(1);
+    //camera2.setResolution(160, 120);
+    //camera2.setFPS(15);
   }
 
   @Override
